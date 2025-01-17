5ad980d7177af22c65304e70949ac9fc3cc74b6f
sleepzc0de <auliyaputraazhari@gmail.com>
2025-01-17 16:33:31 +0700

    FIX BUG

--- resources/views/auth/romadan_login.blade.php
+++ resources/views/auth/romadan_login.blade.php
@@ -2,7 +2,6 @@
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Kementerian Keuangan</title>
