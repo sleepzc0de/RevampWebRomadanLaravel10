0a97308ee5d1ee1d2c23018f8471f8b308027d77
sleepzc0de <auliyaputraazhari@gmail.com>
2025-01-18 19:21:04 +0700

    UPDATE ROUTE PAKE AUTH.PHP SAJA

--- routes/web.php
+++ routes/web.php
@@ -40,14 +40,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::middleware(['web','throttle:6,1'])->group(function () {
-    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-        ->name('login');
-    Route::get('/login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
-});
-
-
 // 1. FRONT END
 Route::group(
     ['prefix' => '/'],
