9977febda7a52e38f4ed40d10ef383c2931a440f
Auliya Putra Azhari <auliyaputraazhari@kemenkeu.go.id>
2025-05-19 09:36:18 +0700

    UPDATE VERSION MINOR 2.2.0

--- config/app.php
+++ config/app.php
@@ -141,7 +141,7 @@ return [
     // Versioning Aplikasi
     'version' => [
         'major' => env('APP_VERSION_MAJOR', '2'),
-        'minor' => env('APP_VERSION_MINOR', '1'),
+        'minor' => env('APP_VERSION_MINOR', '2'),
         'patch' => env('APP_VERSION_PATCH', '0'),
         'release' => env('APP_VERSION_RELEASE', 'stable'), // alpha, beta, rc, stable
         'build' => env('APP_VERSION_BUILD', '001'),
