7e0f536c1ff71120b13c2be9ea98ba35f412c13b
sleepzc0de <auliyaputraazhari@gmail.com>
2026-06-04 08:59:58 +0700

    UPDATE LOG BUG

--- config/logging.php
+++ config/logging.php
@@ -66,8 +66,8 @@ return [
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
+            'level' => env('LOG_LEVEL', 'error'),
+            'days' => 1,
         ],
 
         'slack' => [
